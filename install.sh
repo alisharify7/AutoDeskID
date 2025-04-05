dd4f4bc5562bd04d06d697daba82b0a074d6c3ae
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2025-04-06 00:24:07 +0330

    update bash script for installtion

--- install.sh
+++ install.sh
@@ -1,2 +1,44 @@
-#! /usr/bin/bash
+#!/bin/bash
 
+set -e
+
+REPO_URL="https://github.com/alisharify7/AutoDeskID"
+ZIP_URL="$REPO_URL/archive/refs/heads/main.zip"
+TMP_DIR="/tmp/autodeskid"
+INSTALL_DIR="$HOME/AutoDeskID"
+SERVICE_DIR="/etc/systemd/system/"
+SERVICE_NAME="autodeskid"
+
+
+echo "🚀 Downloading AutoDeskID from GitHub..."
+mkdir -p "$TMP_DIR"
+mkdir -p "$INSTALL_DIR"
+cd "$TMP_DIR"
+curl -L "$ZIP_URL" -o autodeskid.zip
+
+echo "📦 Extracting project..."
+unzip -qo autodeskid.zip
+
+echo "📦 install requirements packges ..."
+mv AutoDeskID-main/* $INSTALL_DIR 
+cd $INSTALL_DIR
+pip3  install -r requirements.txt --break-system-packages
+rm $TMP_DIR -rf
+
+
+sed -i "s|path/to/script/python/file|$INSTALL_DIR/src/main.py|g" systemd.service/autodeskid.service
+sed -i "s|path/to/script/folder|$INSTALL_DIR|g" systemd.service/autodeskid.service
+
+cp systemd.service/autodeskid.service /etc/systemd/system
+
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable autodeskid.service
+
+sudo systemctl start autodeskid.service
+
+sudo systemctl status autodeskid.service
+
+
+echo "✅ Installation complete!"
